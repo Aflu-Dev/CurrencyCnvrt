a92d414d708d0ad9bb71a20a593e80da4f7aae3a
Aflu-Dev <64547564+Aflu-Dev@users.noreply.github.com>
2020-04-29 23:29:44 +0700

    Add files via upload

---
+++ hehe.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CSharp_Shell
+{
+
+    public static class Program 
+    {
+        public static void Main() 
+        {
+           Console.WriteLine("What is your currency?, USD or EURO");
+           string a = Console.ReadLine();
+           if (a == "USD") 
+           {
+           	Console.Write("Input USD value: ");
+           	float input = float.Parse()
+           	
+           }
+        }
+    }
+}
